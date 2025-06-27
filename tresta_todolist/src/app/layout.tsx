d6aae5e062e33311fa381c0b7651a669a0917910
Monika H J <monikahj7@gmail.com>
2025-06-27 17:29:06 +0530

    done

--- tresta_todolist/src/app/layout.tsx
+++ tresta_todolist/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../../src/component/Header"
-import Head from "next/head";
 import {TaskProvider} from "@/context/TaskContext"
 
 const geistSans = Geist({
